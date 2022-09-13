60dae97a2925d9e51ce24a4c7633332341a77f30
Pham Van Hoan <phamvanhoan@luvina.net>
2022-09-13 13:30:43 +0700

    update

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="header flex items-center justify-center gap-x-5 mb-5">
-        <NavLink to="/" className={({isActive}) => (isActive ? "text-primary border-none rounded-md bg-blue-700 p-2" : "")}>Home</NavLink>
+        <NavLink to="/react-movie/" className={({isActive}) => (isActive ? "text-primary border-none rounded-md bg-blue-700 p-2" : "")}>Home</NavLink>
         <NavLink to="/movies" className={({isActive}) => (isActive ? "text-primary border-none rounded-md bg-blue-700 p-2" : "")}>Movies</NavLink>
     </header>
   );
